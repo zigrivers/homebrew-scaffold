dc7681279a225ec89ad303f607f5cb6db26038f8
Ken Allred <zigrivers@gmail.com>
2026-03-17 16:33:55 -0600

    feat: add scaffold formula v2.1.0

---
+++ Formula/scaffold.rb
@@ -0,0 +1,18 @@
+class Scaffold < Formula
+  desc "AI-powered software project scaffolding pipeline"
+  homepage "https://github.com/zigrivers/scaffold"
+  url "https://github.com/zigrivers/scaffold/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "a22f7b97c9b3cae7f36a372a7659d1bd66c954f903a5325cb0cb826e255b8869"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "scaffold", shell_output("#{bin}/scaffold --version")
+  end
+end
